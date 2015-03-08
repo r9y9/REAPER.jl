3af2d8d84908d8677bc7e3bed21d112e3f0d2bcc
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-03-09 00:53:06 +0900

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
 using REAPER
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+x = int16(vec(readdlm(joinpath(dirname(@__FILE__), "test16k.txt"))))
+const fs = 16000
+@assert length(x) == 60700
+@assert isa(x, Vector{Int16})
+
+pm_times, pm, f0_times, f0, corr = reaper(x, fs)
+@test !any(isnan(pm_times))
+@test !any(isnan(pm))
+@test !any(isnan(f0_times))
+@test !any(isnan(f0))
+@test !any(isnan(corr))
+@test length(pm_times) == length(pm)
+@test length(f0_times) == length(f0) == length(corr)
