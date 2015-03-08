43cdfbd7b02e99d561d4fe8ad696eb7b991e70b8
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-03-09 00:52:55 +0900

    Add build script

---
+++ deps/build.jl
@@ -0,0 +1,23 @@
+# build REAPER as a shared library
+
+if !isdir(joinpath(dirname(@__FILE__), "REAPER"))
+    cd(Pkg.dir("REAPER"))
+    run(`git submodule update --init --recursive`)
+end
+
+build_dir = joinpath(Pkg.dir("REAPER"), "deps", "REAPER", "build")
+!isdir(build_dir) && mkdir(build_dir)
+
+cd(build_dir)
+
+run(`cmake ..`)
+run(`make`)
+
+# After runing make, REAPER/build/libreaper.so/dylib will be created
+try
+    dlopen(joinpath(build_dir, "libreaper"))
+catch e
+    rethrow(e)
+end
+
+info("successfully compiled")
